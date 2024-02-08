64e38bd2eefb08df0105ce6a906fc557183a82b6
frozenproof <trucling222@gmail.com>
2024-02-08 14:27:34 +0700

    fixed db.ts where import is coming from wrong directory and not recognizing the models implmented

--- app/(setup)/page.tsx
+++ app/(setup)/page.tsx
@@ -1,4 +1,18 @@
+import { db } from "@/lib/db";
+import { initialProfile } from "@/lib/initial-profile";
+import { UserProfile } from "@clerk/nextjs";
+
 const SetupPage = async () => {
+    const newinitialprofile = await initialProfile();
+    const servers = await db.server.findFirst({
+        where: {
+            members:{
+                some:{
+                    UserProfileId: UserProfile.
+                }
+            }
+        }
+    })
     return ( 
         <div>
             Create a server
--- lib/initial-profile.ts
+++ lib/initial-profile.ts
@@ -3,9 +3,29 @@ import { currentUser, redirectToSignIn } from "@clerk/nextjs";
 import { db } from "@/lib/db"
 
 export const initialProfile = async () => {
+
     const user = await currentUser();
     if(!user) {
         return redirectToSignIn();
-    }
+    };
+
+    const profileInitial = await db.userProfile.findUnique({
+        where:{
+            userId: user.id
+        }
+    })
+
+    if(profileInitial)
+        return profileInitial;
     
+    const newProfileInitial = await db.userProfile.create({
+        data:{
+            userId: user.id,
+            name: `${user.firstName} ${user.lastName}`,
+            imageUrl: user.imageUrl,
+            email: user.emailAddresses[0].emailAddress
+        }
+    }) ;
+
+    return newProfileInitial;
 }
