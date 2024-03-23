9665c7ecdaefcb304a3efcf209f11644906baec8
frozenproof <trucling222@gmail.com>
2024-03-23 16:26:32 +0700

    updated not found page to actually do something

---
+++ app/not-found.tsx
@@ -0,0 +1,19 @@
+import { EvervaultCard } from "@/components/effects/EvervaultCard";
+
+export default function Custom404() {
+
+    
+    return (
+
+      <div
+        className="w-full h-full"
+      >
+        <EvervaultCard
+          className="w-full h-full"
+        >
+        <h1>404 - Page Not Found</h1>
+        </EvervaultCard>
+      </div>
+      
+    )
+  }
