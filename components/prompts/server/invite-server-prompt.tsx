da9c8d325644fd481ac0ebe43cd26c53a1b3fcdb
frozenproof <trucling222@gmail.com>
2024-06-16 17:39:37 +0700

    final fix

--- components/prompts/server/invite-server-prompt.tsx
+++ components/prompts/server/invite-server-prompt.tsx
@@ -14,9 +14,10 @@ import { Input } from "../../ui/input";
 import { Button } from "../../ui/button";
 import { Check, Copy, Plus, RefreshCw } from "lucide-react";
 import { useOrigin } from "@/hooks/use-origin";
-import { useState, Suspense } from "react";
+import { useState, Suspense, useEffect } from "react";
 import axios from "axios";
 import { LoadingMainPage } from "@/components/uihelper/loading-wait";
+import QRCode from "@/components/extra/url/url";
 
 
 export const InviteServerPrompt = () => {
@@ -35,6 +36,7 @@ export const InviteServerPrompt = () => {
     const [ isUrlLoading, setIsUrlLoading ] = useState(false);
 
     const [ realUrl, setRealUrl ] = useState("Click plus to get invite link!");
+    const [ realUrl2, setRealUrl2 ] = useState("Click plus to get invite link!");
 
     const onCopy = () => {
         navigator.clipboard.writeText(realUrl);
@@ -48,7 +50,7 @@ export const InviteServerPrompt = () => {
     const onNew = async() => {
         try {
             setIsLoading(true);
-            const temp = await axios.patch(`/api/invite/inviteServerGenerate`, {serverId: server?.id}).then((response) => setRealUrl(`${origin}/invite-euphoria/`+response.data.inviteCode));
+            const temp = await axios.patch(`/api/invite/inviteServerGenerate`, {serverId: server?.id}).then((response) => {setRealUrl(`${origin}/invite-euphoria/`+response.data.inviteCode); setRealUrl2(response.data.inviteCode);})
             
             // onOpen("InviteServer", {server: response.data});
         }
@@ -65,7 +67,7 @@ export const InviteServerPrompt = () => {
         try {
             console.log(server?.id);
             setIsUrlLoading(true);
-            const temp = await axios.patch(`/api/invite/inviteServerSearch`, {serverId: server?.id}).then((response) => setRealUrl(`${origin}/invite-euphoria/`+response.data.inviteCode));
+            const temp = await axios.patch(`/api/invite/inviteServerSearch`, {serverId: server?.id}).then((response) => {setRealUrl(`${origin}/invite-euphoria/`+response.data.inviteCode); setRealUrl2(response.data.inviteCode);})
 
             console.log("Information is here",temp);
 
@@ -77,77 +79,89 @@ export const InviteServerPrompt = () => {
             setIsUrlLoading(false);
         }
     }
+    useEffect(() => {
+        if (isPromptOpen) {
+            fetchRealUrl();
+        }
+    }, [isPromptOpen]);
 
-    return ( 
-        <Dialog open = {isPromptOpen} onOpenChange={onClose}>
-            <DialogContent className="bg-white text-black p-0 overflow-hidden">
-                <DialogHeader className="pt-8 px-6">
-                    <DialogTitle className="text-2xl text-center font-bold">
-                        Invite anyone right now
-                    </DialogTitle>
-                    <DialogDescription className="text-center">
-                        Games are more fun with friends !
-                    </DialogDescription>
-                </DialogHeader>
-                <div className=" text-center p-8">
-                    <Label className="uppercase text-xs font-bold text-zinc-500 dark:text-secondary/80 items-center ">
-                    Server invite link
-                    </Label>
-                    <div className="flex items-center mt-2 gap-x-2">
-                        <Suspense
-                            fallback={<LoadingMainPage />}
+    if(server)
+    {
+        return ( 
+            <Dialog open = {isPromptOpen} onOpenChange={onClose}>
+                <DialogContent className="bg-white text-black p-0 overflow-hidden">
+                    <DialogHeader className="pt-8 px-6">
+                        <DialogTitle className="text-2xl text-center font-bold">
+                            Invite anyone right now
+                        </DialogTitle>
+                        <DialogDescription className="text-center">
+                            Games are more fun with friends !
+                        </DialogDescription>
+                    </DialogHeader>
+                    <div className=" text-center p-8 items-center flex flex-col w-full">
+                    <div>
+                        <QRCode data={realUrl2} />
+                    </div>   
+                        <Label className="uppercase text-xs font-bold text-zinc-500 dark:text-secondary/80 items-center ">
+                        Server invite link
+                        </Label>
+                        <div className="flex items-center mt-2 gap-x-2 w-full">
+                            <Suspense
+                                fallback={<LoadingMainPage />}
+                            >
+                                <Input
+                                    disabled={isLoading}
+                                    className="bg-zinc-200/80 border-0 
+                                    focus-visible:ring-2 text-[#db8bca]
+                                    focus-visible:ring-offset-0 w-full"
+                                    value={realUrl}
+                                    readOnly
+                                />
+                            </Suspense>
+                            <Suspense>
+    
+                            </Suspense>
+                            <Button
+                            disabled={isLoading || copied || isUrlLoading}
+                            onClick={onCopy} 
+                            size="sm"
+                            className="ml-auto bg-[#cc8c43] hover:bg-[#cc8c48]"
                         >
-                            <Input
+                            {copied ? <Check /> : <Copy />}                    
+                        </Button>
+                        <Button
+                            name="inviteUrlLoad"
+                            disabled={isUrlLoading}
+                            onClick={fetchRealUrl} 
+                            size="sm"
+                            className="ml-auto bg-[#cc8c43] hover:bg-[#cc8c48]"
+                            
+                        >
+                            {isUrlLoading ? <Check /> : <Plus />}                    
+                        </Button>
+                         
+                        </div>
+                        {/* <Input
                                 disabled={isLoading}
-                                className="bg-zinc-200/80 border-0 
+                                className="mt-2 bg-zinc-200/80 border-0 
                                 focus-visible:ring-2 text-[#db8bca]
                                 focus-visible:ring-offset-0"
-                                value={realUrl}
+                                value={server?.inviteCode}
                                 readOnly
-                            />
-                        </Suspense>
-                        <Suspense>
-
-                        </Suspense>
-                        <Button
-                        disabled={isLoading || copied}
-                        onClick={onCopy} 
-                        size="sm"
-                        className="ml-auto bg-[#cc8c43] hover:bg-[#cc8c48]"
-                    >
-                        {copied ? <Check /> : <Copy />}                    
-                    </Button>
-                    <Button
-                        name="inviteUrlLoad"
-                        disabled={isUrlLoading}
-                        onClick={fetchRealUrl} 
-                        size="sm"
-                        className="ml-auto bg-[#cc8c43] hover:bg-[#cc8c48]"
-                        
-                    >
-                        {isUrlLoading ? <Check /> : <Plus />}                    
-                    </Button>
-                     
-                    </div>
-                    {/* <Input
+                            /> */}
+                        <Button 
                             disabled={isLoading}
-                            className="mt-2 bg-zinc-200/80 border-0 
-                            focus-visible:ring-2 text-[#db8bca]
-                            focus-visible:ring-offset-0"
-                            value={server?.inviteCode}
-                            readOnly
-                        /> */}
-                    <Button 
-                        disabled={isLoading}
-                        onClick={onNew}
-                        variant="ghost"
-                        className="text-xs mt-4" 
-                    >
-                        Generate a new invite link
-                        <RefreshCw className="ml-2 mr-auto" />
-                    </Button>
-                </div>
-             </DialogContent>
-        </Dialog>
-    )
+                            onClick={onNew}
+                            variant="ghost"
+                            className="text-xs mt-4" 
+                        >
+                            Generate a new invite link
+                            <RefreshCw className="ml-2 mr-auto" />
+                        </Button>
+                    </div>
+                 </DialogContent>
+            </Dialog>
+        )
+    }
+    
 }
